99cc0356ddb2780174a4eb57330b0cc99b42c991
Brian Simoni <simonibc@vcu.edu>
2016-01-24 22:48:27 -0500

    First iteration of the game

--- js/app.js
+++ js/app.js
@@ -1,7 +1,82 @@
 /**
  * Created by brian on 1/24/16.
  */
-console.log('test');
 
-var disk = $('.disk');
-console.log(disk);
+$(document).ready(function(){
+
+    var moveCount = 0;
+    // add and remove the selected class on click
+    $('.disk').click(function(){
+        $('.disk').removeClass('selected');
+       $(this).addClass('selected');
+    });
+
+
+    $('.tower').click(function(){
+        var clickedTower = $(this);
+
+        var fromTower = {};
+        if ( $('.selected').length == true){
+            var hasSelected = $('.tower').has('.selected');
+            var id = hasSelected[0].id;
+            fromTower = $('#' + id);
+        }
+
+        if ( ( clickedTower.has('.selected').length == false ) && ($('.selected').length == true) ){
+            moveDisk($('.selected'), fromTower, clickedTower);
+        }
+    });
+
+    // moves the selected disk to the tower clicked by the user
+    function moveDisk(disk, fromTower, toTower){
+
+        var diskNumber = disk.text();
+
+        //todo: figure out why I can't use jQuery's first() method
+        // the next three lines get the disk number of the top disk on the clicked tower
+        var toTowerId = '#' + $(toTower).attr('id');
+        var toTowerChildren = $(toTowerId).children();
+        var toTowerDiskNumber = $(toTowerChildren[0]).text();
+
+        // the next three lines get the disk number of the top disk on the from tower
+        var fromTowerId = '#' + $(fromTower).attr('id');
+        var fromTowerChildren = $(fromTowerId).children();
+        var fromTowerDiskNumber = $(fromTowerChildren[0]).text();
+
+        if(diskNumber < toTowerDiskNumber || diskNumber < fromTowerDiskNumber){
+            alert('illegal move');
+        } else{
+            $(toTower).prepend(disk);
+            moveCount++;
+            console.log('Move Count ' + moveCount);
+        }
+
+
+    }
+
+});
+
+
+
+function resetGame(numDisks){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
